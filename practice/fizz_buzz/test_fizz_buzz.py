4aa2645eeee638a3311c9d16946f52b13844f45e
candacebeall <cduke1313@gmail.com>
2018-11-01 23:36:02 -0400

    Fix format

--- practice/fizz_buzz/test_fizz_buzz.py
+++ practice/fizz_buzz/test_fizz_buzz.py
@@ -98,8 +98,7 @@ def test_fizz_buzz_fifteen():
 
 def test_fizz_buzz_thirty():
     expected = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz',
-                16, 17, 'fizz', 19, 'buzz', 'fizz', 22, 23, 'fizz', 'buzz', 26, 'fizz', 28, 29, 'fizzbuzz'
-                ]
+                16, 17, 'fizz', 19, 'buzz', 'fizz', 22, 23, 'fizz', 'buzz', 26, 'fizz', 28, 29, 'fizzbuzz']
 
     actual = fizz_buzz(30)
 
