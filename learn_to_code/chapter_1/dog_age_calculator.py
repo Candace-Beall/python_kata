a595821d233502503cfa904db3c65b0ffac09031
candacebeall <cduke1313@gmail.com>
2018-11-02 22:40:45 -0400

    Add Dog Age Calculator

---
+++ learn_to_code/chapter_1/dog_age_calculator.py
@@ -0,0 +1,9 @@
+print('Dog Age')
+
+name = input("What is your dog's name? ")
+
+age = input("What is your dog's age? ")
+
+dog_age = int(age) * 7
+
+print("Your Dog's Name is " + name + ", its age is " + str(dog_age) + '.')
