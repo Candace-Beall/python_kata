90cb7fa282b80adc7092de6e1b691a9b171051bc
candacebeall <cduke1313@gmail.com>
2019-02-21 13:06:45 -0500

    Add second rock paper scissors

---
+++ learn_to_code/chapter_3/rpc.py
@@ -0,0 +1,10 @@
+print('Justin!')
+
+color = 'blue'
+guess = ''
+guesses = 0
+
+while guess !=
+    guess = input('What color am I thinking of? ')
+    guesses = guesses + 1
+print('You got it! It took you', guesses, 'guesses')
