e37d50b888cd837f05158d53fd80c00b6c1d5efb
candacebeall <cduke1313@gmail.com>
2019-02-21 13:09:10 -0500

    Remove syntax error

--- learn_to_code/chapter_3/rpc.py
+++ learn_to_code/chapter_3/rpc.py
@@ -4,7 +4,7 @@ color = 'blue'
 guess = ''
 guesses = 0
 
-while guess !=
+while color != guess:
     guess = input('What color am I thinking of? ')
     guesses = guesses + 1
 print('You got it! It took you', guesses, 'guesses')
